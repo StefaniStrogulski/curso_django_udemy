772a00ec87c168c7cb1343db0f62461ebdc016da
stefani_stogulski <stefanistrogulski@hotmail.com>
2022-07-03 23:11:04 -0300

    Removido contato e sobre. Alterei a pasta base_templates para fora da pasta de projetos.

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Stefani',
+        'name': 'Stefani de Souza Strogulski',
     })
-
-
-def sobre(request):
-    return HttpResponse('sobre 1')
-
-
-def contato(request):
-    return HttpResponse('contato 1')
