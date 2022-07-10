43fd2b6ac5fbc8eb7badf97001e59b16b4b7a661
stefani_stogulski <stefanistrogulski@hotmail.com>
2022-07-10 12:34:10 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Stefani de Souza Strogulski',
+        'recipes':[make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Stefani de Souza Strogulski',
+        'recipe': make_recipe(),
     })
 
